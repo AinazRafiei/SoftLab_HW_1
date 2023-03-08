f2f3fbf77b65d848e8798f342aa4a6bb2cbd5d7d
salehshoji <mohamadsaleh.shojai@yahoo.com>
2023-03-08 16:45:10 +0330

    add response for help and pure text

--- telegram_bot.py
+++ telegram_bot.py
@@ -11,8 +11,20 @@ URL = f'https://api.telegram.org/bot{TOKEN}/getFile?file_id='
 bot = telebot.TeleBot(TOKEN)
 
 
-@bot.message_handler(commands=['start', 'hello'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "سلام!")
+    bot.reply_to(message, "سلام!"
+                          "\n چطور میتونم به شما کمک کنم؟")
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.send_message(message.chat.id, "هر چیزی که میخوای تایپ کن!")
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    bot.reply_to(message, message.text)
+
 
 bot.infinity_polling()
