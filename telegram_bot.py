d8aac6dda8664ff3a998979fe628dd59525672ff
salehshoji <mohamadsaleh.shojai@yahoo.com>
2023-03-08 16:06:52 +0330

    bot say hi

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,1 +1,18 @@
-#todo telegeram bot implement here
+# todo telegeram bot implement here
+
+import os
+import random
+import logging
+import telebot
+
+TOKEN = '6261438264:AAHYs2yUmBmISRrMWQBKrTdmH-_1xARawyg'
+URL = f'https://api.telegram.org/bot{TOKEN}/getFile?file_id='
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "سلام!")
+
+bot.infinity_polling()
